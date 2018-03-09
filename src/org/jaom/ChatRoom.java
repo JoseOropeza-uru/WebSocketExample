07a8df184719484417d80b782188467b33ce7e50
JoseOropeza-uru <joseoropeza@uru.edu>
2018-03-08 22:04:57 -0400

    changes to code

--- src/org/jaom/ChatRoom.java
+++ src/org/jaom/ChatRoom.java
@@ -12,6 +12,8 @@ import javax.websocket.OnOpen;
 import javax.websocket.Session;
 import javax.websocket.server.ServerEndpoint;
 
+import org.json.JSONObject;
+
 @ServerEndpoint("/chat")
 public class ChatRoom {
 	private static Set<Session> clients = 
@@ -21,10 +23,12 @@ public class ChatRoom {
 	@OnMessage
 	public void onMessage (String msg, Session session)
 			throws IOException{
-		
+		JSONObject json = new JSONObject();
+		json.put("status", 200).put("msg", msg);
+	
 		synchronized (clients) {
 			for (Session client : clients) {
-				client.getBasicRemote().sendText(msg);
+					client.getBasicRemote().sendText(json.toString());
 			}
 		}
 		
@@ -32,16 +36,17 @@ public class ChatRoom {
 	
 	@OnOpen
 	public void onOpen(Session session) throws IOException{
-		System.out.println("b");
-		clients.add(session);
+			clients.add(session);
 	}
 	
 	@OnClose
 	public void onClose (Session session) throws IOException{
 		clients.remove(session);
+		JSONObject json = new JSONObject();
+		json.put("status", 200).put("msg", "Alguien abandono la sala");
 		synchronized (clients) {
 			for (Session client : clients) {
-				client.getBasicRemote().sendText( "Alguien abandono la sala");
+				client.getBasicRemote().sendText( json.toString());
 			}
 		}
 		
@@ -49,9 +54,11 @@ public class ChatRoom {
 	@OnError
 	public void onError(Throwable e) throws IOException {
 	    e.printStackTrace();
+		JSONObject json = new JSONObject();
+		json.put("status", 500).put("msg", e.getStackTrace());
 	    synchronized (clients) {
 			for (Session client : clients) {
-				client.getBasicRemote().sendText( e.toString());
+				client.getBasicRemote().sendText( json.toString());
 			}
 		}
 	}
